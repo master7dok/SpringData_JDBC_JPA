6b406a39558c3e31f13ef58962aa51b81e682807
Mykhailo Babiuk <master7dok@gmail.com>
2023-01-31 21:17:53 +0100

    Create Account repository interfase with JPA and test Account

--- src/main/java/org/example/com/mbdev/springdata/Application.java
+++ src/main/java/org/example/com/mbdev/springdata/Application.java
@@ -1,6 +1,7 @@
 package org.example.com.mbdev.springdata;
 
 import org.example.com.mbdev.springdata.entity.Account;
+import org.example.com.mbdev.springdata.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -17,16 +18,24 @@ public class Application implements CommandLineRunner {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
-
+    @Autowired
+    private AccountRepository accountRepository;
     @Override
     public void run(String... args) throws Exception {
-        jdbcTemplate.execute("INSERT INTO Account (id, name, email, bill)" +
-                                    "VALUES (1, 'Lori', 'lori@cat.ua', 2000)");
+//        jdbcTemplate.execute("INSERT INTO Account (id, name, email, bill)" +
+//                                    "VALUES (1, 'Lori', 'lori@cat.ua', 2000)");
+//
+//        Map<String, Object> resultSet = jdbcTemplate.queryForMap("SELECT * from Account");
+//        System.out.println(resultSet.get("email"));
+//        Account accountById = findAccountById(1L);
+//        System.out.println(accountById);
+        for (Long i = Long.valueOf(1); i < 10; i++) {
+            accountRepository.save(new Account(null, "Lori" + i, "lori@cat.ua", (int) (2000*i)));
+        }
 
-        Map<String, Object> resultSet = jdbcTemplate.queryForMap("SELECT * from Account");
-        System.out.println(resultSet.get("email"));
-        Account accountById = findAccountById(1L);
-        System.out.println(accountById);
+        System.out.println(accountRepository.findAccountByBill(4000));
+        System.out.println(accountRepository.findAccountByName("Lori5"));
+        System.out.println(accountRepository.findAccountByNameAndBill("Lori6", 12000+));
     }
     private Account findAccountById(Long accountId) {
         String query = "SELECT * FROM Account WHERE id=%s";
---
+++ src/main/java/org/example/com/mbdev/springdata/repository/AccountRepository.java
@@ -0,0 +1,16 @@
+package org.example.com.mbdev.springdata.repository;
+
+import org.example.com.mbdev.springdata.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    Account findAccountByBill(Integer bill);
+    Account findAccountByName(String name);
+    @Query(nativeQuery = true, value = "select * from Account where name = :name and bill = :bill")
+    Account findAccountByNameAndBill(@Param("name") String name, @Param("bill") Integer bill);
+
+
+}
